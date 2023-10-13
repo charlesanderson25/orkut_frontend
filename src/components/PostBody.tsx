d0b9f289cd52128678fb353d91aad36a995b24a9
charlesanderson25 <charles_spfc@hotmail.com>
2023-10-13 16:16:50 -0300

    Change name file

---
+++ src/components/PostBody.tsx
@@ -0,0 +1,11 @@
+
+// Componente Pai
+const NotepadBody = () => {
+    return (
+      <div className="bg-backGroundColorDarkTheme h-screen">
+        <NotepadBody/>
+      </div>
+    );
+  };
+  
+  export default NotepadBody;
