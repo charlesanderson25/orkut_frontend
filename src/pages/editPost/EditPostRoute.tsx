fd73f5da65ae9402913fadefdabe4cec85ae0036
charlesanderson25 <charles_spfc@hotmail.com>
2023-10-21 11:50:32 -0300

    Add fragment on return

--- src/pages/editPost/EditPostRoute.tsx
+++ src/pages/editPost/EditPostRoute.tsx
@@ -9,6 +9,7 @@ import toast from "react-simple-toasts";
 import { useEffect, useState } from "react";
 import BreadCrumbs from "../../components/BreadCrumbs";
 import { Helmet } from "react-helmet";
+import { Card } from "../../components/Card";
 
 interface StyledLabelProps {
   titulo: string;
@@ -76,37 +77,38 @@ const EditPostRoute = () => {
   }, [params.id]);
 
   return (
-    <section className="my-20 relative min-h-screen">
-      <Helmet>
-        <title>{textEditPost.titleEdit}</title>
-      </Helmet>
-      <div className="form-container absolute inset-x-1/4">
-        <form
-          className="form bg-darkTheme rounded-2xl py-9 px-16 font-sans"
-          ref={zo.ref}
-        >
-          <div className="text-white pl-3 pb-1">
-            <BreadCrumbs
-              link={[
-                { href: "/", label: "Home" },
-                {
-                  href: "/listar-posts/",
-                  label: "Listar Posts",
-                },
-              ]}
-            />
-          </div>
-          <h1 className="font-bold text-2xl text-white ml-3">
-            {textEditPost.title} {params.id}
-          </h1>
+    <>
+      <section className="my-20 relative min-h-screen">
+        <Helmet>
+          <title>{textEditPost.titleEdit}</title>
+        </Helmet>
+        <div className="form-container absolute inset-x-1/4">
+          <form
+            className="form bg-darkTheme rounded-2xl py-9 px-16 font-sans"
+            ref={zo.ref}
+          >
+            <div className="text-white pl-3 pb-1">
+              <BreadCrumbs
+                link={[
+                  { href: "/", label: "Home" },
+                  {
+                    href: "/listar-posts/",
+                    label: "Listar Posts",
+                  },
+                ]}
+              />
+            </div>
+            <h1 className="font-bold text-2xl text-white ml-3">
+              {textEditPost.title} {params.id}
+            </h1>
 
-          <div className="flex-col">
-            <StyledLabel
-              titulo="Título"
-              subtitulo="Subtítulo"
-              texto="Texto"
-            ></StyledLabel>
-            {/* <input
+            <div className="flex-col">
+              <StyledLabel
+                titulo="Título"
+                subtitulo="Subtítulo"
+                texto="Texto"
+              ></StyledLabel>
+              {/* <input
               className={`bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md text-white ${zo.errors.title(
                 "border-2 border-red-500 focus:border-red-600"
               )} `}
@@ -115,19 +117,19 @@ const EditPostRoute = () => {
               name={zo.fields.title()}
               defaultValue={initialStageFormEditPost.title}
             /> */}
-            {zo.errors.title((erro) => (
+              {/* {zo.errors.title((erro) => (
               <ErrorMessage>{erro.message}</ErrorMessage>
-            ))}
-          </div>
+            ))} */}
+            </div>
 
-          <div className="flex-col">
-            <StyledLabel
-              titulo="Título"
-              subtitulo="Subtítulo"
-              texto="Texto"
-            ></StyledLabel>
+            <div className="flex-col">
+              <StyledLabel
+                titulo="Título"
+                subtitulo="Subtítulo"
+                texto="Texto"
+              ></StyledLabel>
 
-            {/* <input
+              {/* <input
               className={`bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md text-white ${zo.errors.subtitle(
                 "border-2 border-red-500 focus:border-red-600"
               )} `}
@@ -137,35 +139,36 @@ const EditPostRoute = () => {
               defaultValue={initialStageFormEditPost.subtitle}
             /> */}
 
-            {zo.errors.subtitle((erro) => (
+              {/* {zo.errors.subtitle((erro) => (
               <ErrorMessage>{erro.message}</ErrorMessage>
-            ))}
-          </div>
+            ))} */}
+            </div>
 
-          <div className="flex-col">
-            <StyledLabel
-              titulo="Título"
-              subtitulo="Subtítulo"
-              texto="Texto"
-            ></StyledLabel>
-            <textarea
-              className={`bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md text-white ${zo.errors.content(
-                "border-2 border-red-500 focus:border-red-600"
-              )} `}
-              id=""
-              placeholder="Digite seu Texto"
-              name={zo.fields.content()}
-              defaultValue={initialStageFormEditPost.content}
-            ></textarea>
+            <div className="flex-col">
+              <StyledLabel
+                titulo="Título"
+                subtitulo="Subtítulo"
+                texto="Texto"
+              ></StyledLabel>
+              <textarea
+                className={`bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md text-white ${zo.errors.content(
+                  "border-2 border-red-500 focus:border-red-600"
+                )} `}
+                id=""
+                placeholder="Digite seu Texto"
+                name={zo.fields.content()}
+                defaultValue={initialStageFormEditPost.content}
+              ></textarea>
 
-            {zo.errors.content((erro) => (
-              <ErrorMessage>{erro.message}</ErrorMessage>
-            ))}
-          </div>
-          <ButtonSubmitForm />
-        </form>
-      </div>
-    </section>
+              {zo.errors.content((erro) => (
+                <ErrorMessage>{erro.message}</ErrorMessage>
+              ))}
+            </div>
+            <ButtonSubmitForm />
+          </form>
+        </div>
+      </section>
+    </>
   );
 };
 
