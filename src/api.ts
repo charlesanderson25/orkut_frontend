515c3fd9a0ea8c4f3b282a680977b86222be8130
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-14 10:02:32 -0300

    Add api.interceptors

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
+import { TokenStorage } from "./tokenStorage";
 
 export const api = axios.create({
   baseURL: "http://localhost:8080/",
+});
+
+api.interceptors.request.use((config) => {
+  const token = TokenStorage.getToken;
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
