e63e09fc6d425c934944da4e1d22a59a01313ccd
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-15 14:52:48 -0300

    Add LoadUser

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -6,19 +6,23 @@ import CreatePostRoute from "../pages/createPostRoute/CreatePostRoute";
 import EditPostRoute from "../pages/editPost/EditPostRoute";
 import PostListMain from "../pages/PostListMain/PostListMain";
 import { SignIn } from "../components/SignIn";
+import { LoadUser } from "../components/LoadUser";
 
 const RoutesApp = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Welcome />} />
-      <Route path="/listar-posts" element={<PostListMain />} />
-      <Route path="/criar-Post" element={<CreatePostRoute />} />
-      <Route path="image" element={<BackGroundImage />} />
-      <Route path="/editar-post/:id" element={<EditPostRoute />} />
-      <Route path="/entrar" element={<SignIn />} />
+    <div>
+      <LoadUser />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/listar-posts" element={<PostListMain />} />
+        <Route path="/criar-Post" element={<CreatePostRoute />} />
+        <Route path="image" element={<BackGroundImage />} />
+        <Route path="/editar-post/:id" element={<EditPostRoute />} />
+        <Route path="/entrar" element={<SignIn />} />
 
-      <Route path="*" element={<NotFoundTwo />} />
-    </Routes>
+        <Route path="*" element={<NotFoundTwo />} />
+      </Routes>
+    </div>
   );
 };
 
