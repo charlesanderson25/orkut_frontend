0cc7843d796e9f2991208b24099a3b9600b05103
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-15 15:42:43 -0300

    Changes file

--- src/components/LoadUser.tsx
+++ src/components/LoadUser.tsx
@@ -1,10 +1,27 @@
 import { useEffect } from "react";
+import { TokenStorage } from "../tokenStorage";
+import { useGlobalStore } from "../useGlobalStore";
 import { api } from "../api";
 
 export function LoadUser() {
+  const setUser = useGlobalStore((state) => state.setUser);
+  const setIsAuthorized = useGlobalStore((state) => state.setIsAuthorized);
+
   async function loadUser() {
+    const token = TokenStorage.getToken();
+    if (!token) {
+      return;
+    }
     const request = await api.get("/auth/session");
-    console.log(request.data);
+    const user = request.data;
+    setIsAuthorized(true);
+    setUser({
+      id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      avatar: user.avatar,
+    });
   }
 
   useEffect(() => {
