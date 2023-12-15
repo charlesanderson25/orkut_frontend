d9e64b1d39091e29d9dc1bd8d081b086b0ba1d02
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-15 15:41:55 -0300

    Add isAuthorized

--- src/useGlobalStore.ts
+++ src/useGlobalStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 
 type GlobalStore = {
+  isAuthorized: boolean;
   user: {
     id: number;
     first_name: string;
@@ -8,9 +9,12 @@ type GlobalStore = {
     email: string;
     avatar: string;
   };
+  setUser: (user: GlobalStore["user"]) => void;
+  setIsAuthorized: (isAuthorized: boolean) => void;
 };
 
 const initialState = {
+  isAuthorized: false,
   user: {
     id: 0,
     first_name: "",
@@ -23,5 +27,15 @@ const initialState = {
 export const useGlobalStore = create<GlobalStore>((set) => {
   return {
     ...initialState,
+    setUser(user) {
+      set({
+        user,
+      });
+    },
+    setIsAuthorized(isAuthorized) {
+      set({
+        isAuthorized,
+      });
+    },
   };
 });
